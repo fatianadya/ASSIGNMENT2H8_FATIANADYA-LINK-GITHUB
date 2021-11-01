b8cd1418c17ad4894a472d99f570508254bd2628
fatianadya <89918362+fatianadya@users.noreply.github.com>
2021-11-01 22:49:51 +0700

    Add files via upload

---
+++ SESI 5/nanad.js
@@ -0,0 +1,11 @@
+function dom1UpdateText(){
+    let FirstNameEl = document.getElementById("First Name")
+    let inputFirstNameEl = document.getElementById("input name")
+    FirstNameEl.innerText = inputFirstNameEl.values
+}
+
+function dom1UpdateText() {
+    let AvailabilityeL = document.getElementById("availability")
+    let inputAvailabilitylEL = document.getElementById("inputAvailability")
+    AvailabilitylEL.innerText = inputAvailabilitylEL.value
+}
